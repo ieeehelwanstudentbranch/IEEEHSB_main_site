dc1bdd5c96a2a0e0e193bdbe308114ce84372cb5
Mohamed Najiub <mohamednajiub@gmail.com>
2020-03-22 10:56:39 +0200

    add new site structure

--- public/js/del.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-// menue-button
-    $("#del_img").click(function () {
-        $(".hero_image").attr("value","Null !important");
-        $(".del_img").toggle();
-    })
-})
